d594e9fc6e921e95e47becdb6ab8090caaa8dc37
William-silva-Developer <william958silva@gmail.com>
2023-07-31 22:37:44 -0300

    Simulador Best-Fit

--- BestFit/script.js
+++ BestFit/script.js
@@ -1,42 +1,47 @@
+function bestFitAllocation(memoryBlocks, blockSizeNeeded) {
+  let bestFitIndex = -1;
+  let smallestRemainingSize = Infinity;
 
+  for (let i = 0; i < memoryBlocks.length; i++) {
+    const blockSize = memoryBlocks[i];
 
-
-function alocarMemoriaBestFit(blocosMemoria, tamanhoNecessario) {
-    let melhorIndice = -1;
-    let menorDiferenca = Infinity;
-  
-    for (let i = 0; i < blocosMemoria.length; i++) {
-      const tamanhoBloco = blocosMemoria[i];
-  
-      if (tamanhoBloco >= tamanhoNecessario) {
-        const diferenca = tamanhoBloco - tamanhoNecessario;
-  
-        if (diferenca < menorDiferenca) {
-          melhorIndice = i;
-          menorDiferenca = diferenca;
-        }
-      }
+    if (blockSize >= blockSizeNeeded && blockSize - blockSizeNeeded < smallestRemainingSize) {
+      bestFitIndex = i;
+      smallestRemainingSize = blockSize - blockSizeNeeded;
     }
-  
-    return melhorIndice;
   }
-  
-  
-  
-  // Exemplo de uso:
-  const blocosMemoria = [100, 50, 200, 80, 150];
-  
-  const tamanhoNecessario = 100;
-  
-  const indiceAlocado = alocarMemoriaBestFit(blocosMemoria, tamanhoNecessario);
-  
-  if (indiceAlocado !== -1) {
-  
-    console.log(`Bloco alocado no índice ${indiceAlocado}`);
-    
+
+  return bestFitIndex;
+}
+/*
+// Exemplo de uso:
+const memoryBlocks = [100, 50, 200, 80, 150];
+const blockSizeNeeded = 100;
+
+const allocatedBlockIndex = bestFitAllocation(memoryBlocks, blockSizeNeeded);
+if (allocatedBlockIndex !== -1) {
+  console.log(`Bloco alocado no índice: ${allocatedBlockIndex}`);
+} else {
+  console.log("Nenhum bloco adequado encontrado.");
+}
+*/
+
+
+
+
+
+function allocateMemory() {
+  const memoryBlocksInput = document.getElementById('memoryBlocksInput').value;
+  const blockSizeNeededInput = document.getElementById('blockSizeNeededInput').value;
+
+  const memoryBlocks = memoryBlocksInput.split(',').map(Number);
+  const blockSizeNeeded = parseInt(blockSizeNeededInput);
+
+  const allocatedBlockIndex = bestFitAllocation(memoryBlocks, blockSizeNeeded);
+
+  if (allocatedBlockIndex !== -1) {
+    document.getElementById('result').textContent = `Bloco alocado no índice: ${allocatedBlockIndex}`;
   } else {
-  
-    console.log("Nenhum bloco adequado encontrado.");
-    
+    document.getElementById('result').textContent = "Nenhum bloco adequado encontrado.";
   }
-  
+}
